b9f1990655af4d6dfffd0ad8c121eb84a4470a91
nada-mah <cutenada.lara4@gmail.com>
2023-12-28 16:06:31 +0200

    new file: 8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT name ,state_id
+    FROM cities 
+    WHERE state_id = (
+        SELECT id 
+        FROM states 
+        WHERE name = "California" 
+        ORDER BY id ASC
+    )
