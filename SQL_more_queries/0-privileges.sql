e876b50f211c2fbbbce8c3963848cf0bc67140ff
nada-mah <cutenada.lara4@gmail.com>
2023-12-28 14:19:26 +0200

    fix spelling issues

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- list the PRIVILEGES of user "user_0d_1"
 
-SHOW GEANTS FOR 'user_0d_1'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost'
 -- list the PRIVILEGES of user "user_0d_2"
 
-SHOW GEANTS FOR 'user_0d_2'@'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost'
